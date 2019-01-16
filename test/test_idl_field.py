ad02293816ba5e5f67b084029b7f4ad6870d0d6a
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2019-01-16 20:34:50 +0100

    Update test_idl_field.py

--- test/test_idl_field.py
+++ test/test_idl_field.py
@@ -21,17 +21,17 @@ class field(unittest.TestCase):
     def test_field__interogate_field_0(self):
         self.idl.run("a = interogate_field('{}/Z3_lsh/','10','-')".format(self.mfl_dir))
         a = self.idl.a
-        self.assertIsInstance(p, int8)
+        self.assertIsInstance(a, int8)
         self.assertEqual(a, 1)
 
     def test_field__interogate_field_1(self):
         self.idl.run("a = interogate_field('{}/VIP4_lat/','10','-')".format(self.mfl_dir))
         a = self.idl.a
-        self.assertIsInstance(p, int8)
+        self.assertIsInstance(a, int8)
         self.assertEqual(a, 0)
     
     def test_field__interogate_field_stop(self):
         self.idl.run("a = interogate_field('{}/VIPAL_lsh/','10','-')".format(self.mfl_dir))
         a = self.idl.a
-        self.assertIsInstance(p, int8)
+        self.assertIsInstance(a, int8)
         self.assertEqual(a, 0)
