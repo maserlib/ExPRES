eedc6889bc147cb7d9705347bdbd5ed8fef506f6
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2019-01-16 20:04:15 +0100

    Update test_idl_field.py

--- test/test_idl_field.py
+++ test/test_idl_field.py
@@ -10,13 +10,27 @@ class field(unittest.TestCase):
 
     def setUp(self):
         self.idl = init_serpe_idl()
-        self.cfg = get_config()
+        self.mfl_dir = get_mfl_dir()
+        check_mfl_name('Z3_lsh')
+        check_mfl_name('VIPAL_lat')
 
     def tearDown(self):
         self.idl.run('.reset_session')
 
     def test_field__interrogate_field_0(self):
-        self.idl.run("a = interrogate_field('support/test/mfl/Z3_lsh/','1','-')")
+        self.idl.run("a = interrogate_field({}Z3_lsh/,'10','-')".format(self.mfl_dir))
         a = self.idl.a
         self.assertIsInstance(p, int8)
         self.assertEqual(a, 1)
+
+    def test_field__interrogate_field_1(self):
+        self.idl.run("a = interrogate_field({}VIPAL_lat/,'10','-')".format(self.mfl_dir))
+        a = self.idl.a
+        self.assertIsInstance(p, int8)
+        self.assertEqual(a, 0)
+    
+    def test_field__interrogate_field_stop(self):
+        self.idl.run("a = interrogate_field({}VIPAL_lsh/,'10','-')".format(self.mfl_dir))
+        a = self.idl.a
+        self.assertIsInstance(p, int8)
+        self.assertEqual(a, 0)
