7b8a56bd7603e9e6237c7fdfb619cef623a02fbd
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2019-01-16 19:56:22 +0100

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,16 @@
 import configparser
 from idlpy import *
 import pathlib
+import urllib.request
+import os
 
 cur_dir = pathlib.Path(__file__).parent
 src_dir = cur_dir.parent / 'src'
+sup_dir = cur_dir.parent / 'support' / 'tests'
+cdawlib_path = '/Users/baptiste/Projets/JUNO/Ground-Support/cdf/cdawlib'
+coyote_path = '/Users/baptiste/Development/idl_lib/coyote'
 
+mfl_url = 'http://maser.obspm.fr/support/expres/mfl/'
 
 def get_config():
     config = configparser.ConfigParser()
@@ -14,9 +20,25 @@ def get_config():
 
 def init_serpe_idl():
     IDL.run("CD, '{}'".format(str(src_dir)))
-    IDL.run('!path = !path + ":" + EXPAND_PATH("+{}") + ":" + EXPAND_PATH("+{}")'.format('/Users/baptiste/Projets/JUNO/Ground-Support/cdf/cdawlib', '/Users/baptiste/Development/idl_lib/coyote'))
+    IDL.run('!path = !path + ":" + EXPAND_PATH("+{}") + ":" + EXPAND_PATH("+{}")'.format(cdawlib_path, coyote_path))
     return IDL
 
 
 def get_test_json_file():
-    return cur_dir.parent / 'support' / 'tests' / 'expres_earth_jupiter_io_jrm09_lossc-wid1deg_3kev_20150430_v01.json'
+    return sup_dir / 'expres_earth_jupiter_io_jrm09_lossc-wid1deg_3kev_20150430_v01.json'
+
+
+def get_test_mfl_dir():
+    return sup_dir / 'mfl'
+
+
+def load_test_mfl(mfl_name):
+    mfl_dir = get_test_mfl_dir()
+    if (mfl_dir / mfl_name).exists():
+        pass
+    else:
+        ; downloading corresponding mfl_name data
+        mfl_url_name = '{}{}.tar.gz'.format(mfl_url,mfl_name)
+        mfl_tmp_tgz = pathlib.Path('/tmp') / '{}.tar.gz'.format(mfl_name)
+        urllib.request.urlretrieve(mfl_url_name, str(mfl_tmp_tgz))
+        os.system('tar -xzf {} {}'.format(str(mfl_tmp_tgz), str(mfl_dir)))
