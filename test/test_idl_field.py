8787c928b0fcc58085da4d5abb18b6f5b67e3fc1
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2019-01-16 20:30:03 +0100

    Update test_idl_field.py

--- test/test_idl_field.py
+++ test/test_idl_field.py
@@ -17,20 +17,20 @@ class field(unittest.TestCase):
     def tearDown(self):
         self.idl.run('.reset_session')
 
-    def test_field__interrogate_field_0(self):
-        self.idl.run("a = interrogate_field('{}/Z3_lsh/','10','-')".format(self.mfl_dir))
+    def test_field__interogate_field_0(self):
+        self.idl.run("a = interogate_field('{}/Z3_lsh/','10','-')".format(self.mfl_dir))
         a = self.idl.a
         self.assertIsInstance(p, int8)
         self.assertEqual(a, 1)
 
-    def test_field__interrogate_field_1(self):
-        self.idl.run("a = interrogate_field('{}/VIP4_lat/','10','-')".format(self.mfl_dir))
+    def test_field__interogate_field_1(self):
+        self.idl.run("a = interogate_field('{}/VIP4_lat/','10','-')".format(self.mfl_dir))
         a = self.idl.a
         self.assertIsInstance(p, int8)
         self.assertEqual(a, 0)
     
-    def test_field__interrogate_field_stop(self):
-        self.idl.run("a = interrogate_field('{}/VIPAL_lsh/','10','-')".format(self.mfl_dir))
+    def test_field__interogate_field_stop(self):
+        self.idl.run("a = interogate_field('{}/VIPAL_lsh/','10','-')".format(self.mfl_dir))
         a = self.idl.a
         self.assertIsInstance(p, int8)
         self.assertEqual(a, 0)
