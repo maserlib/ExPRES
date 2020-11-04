632e8ad4c9fe9802ae499e74b6d7adc299ec862f
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2020-11-04 09:19:58 +0100

    removed unused extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,6 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx-jsonschema',
     'sphinx_rtd_theme',
     'sphinxcontrib.bibtex',
 ]
