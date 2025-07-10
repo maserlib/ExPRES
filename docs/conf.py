dafdf096bb9c331187887ac102436cfea25d70a3
CorentinLouis <corentin.louis@obspm.fr>
2025-07-10 14:58:36 +0200

    Updating release version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Louis, CK; Cecconi, B; Hess SLG; Loh, A'
 author = 'Louis, CK; Cecconi, B; Hess SLG'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.4'
 
 
 # -- General configuration ---------------------------------------------------
